bcfa858a755df9f7c2d343c75f1c261ca0433f89
jonathan-bug <jonathanlima.dev@gmail.com>
2024-08-16 12:52:43 -0400

    Working auth

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,14 +2,7 @@ import pg from "pg";
 const { Pool } = pg;
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-    sslmode: process.env.DB_SSL
-});
+    connectionString: process.env.DB_CONN
+})
 
 export default pool;
